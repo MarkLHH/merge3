e64910b36827b2405bae924c5ff9ae2ee6f7cf14
MarkLHH <lhhmark@gmail.com>
2025-02-01 00:25:49 +0800

    grid with pattern size is 7 for default

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import test
 
 def main():
     try:
-        new_game = grid.grid(4)
+        new_game = grid.grid(7)
         new_game.game_round()
     except KeyboardInterrupt:
         print(console_util.center_text(f"Game terminated!"))
