d398dfa83ddf0701381ca4879ac06f62c7741d84
MarkLHH <lhhmark@gmail.com>
2025-02-01 17:49:26 +0800

    remove field.py

--- field.py
+++
@@ -1,8 +0,0 @@
-class field():
-    def __init__(self, x, y):
-        self.x_pos = x
-        self.y_pos = y
-        self.content = '.'
-    
-    def field_content(self):
-        return self.content
